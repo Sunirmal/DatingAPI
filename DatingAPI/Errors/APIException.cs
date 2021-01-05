55bfa04da91f8383f795304abb65cc79176041bc
sunirmal <sunirmal.sikder@outlook.com>
2021-01-05 11:16:46 +0530

    Added Custom Exception

---
+++ DatingAPI/Errors/APIException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace DatingAPI.Errors
+{
+    public class APIException
+    {
+        public APIException(int statusCode,
+            string message= null,
+            string detail=null)
+        {
+            this.StatusCode     = statusCode;
+            this.Message        = message;
+            this.Detail         = detail;
+        }
+
+        public int StatusCode { get; set; }
+        public string Message { get; set; }
+        public string Detail { get; set; }
+    }
+}
---
+++ DatingAPI/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DatingAPI.Errors;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace DatingAPI.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _environment;
+
+        public ExceptionMiddleware(RequestDelegate next,
+            ILogger<ExceptionMiddleware> logger, IHostEnvironment environment)
+        {
+            this._next = next;
+            this._logger = logger;
+            this._environment = environment;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            //return await _next(httpContext);
+            try
+            {
+                await this._next(context);
+
+            }catch(Exception exception)
+            {
+                this._logger.LogError(exception, exception.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                var response = this._environment.IsDevelopment() ?
+                            new APIException(context.Response.StatusCode, exception.Message,exception.StackTrace?.ToString()) :
+                            new APIException(context.Response.StatusCode, "Internal Server Error");
+                var options = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+                var json = JsonSerializer.Serialize(response, options);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
