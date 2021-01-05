4fa349786f7e974ba336df2c4df19a896d1b0367
sunirmal <sunirmal.sikder@outlook.com>
2021-01-05 12:49:55 +0530

    Merge changes

---
+++ DatingAPI/Controllers/BuggyController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPI.Data;
+using DatingAPI.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DatingAPI.Controllers
+{
+
+    public class BuggyController : BaseController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            this._context = context;
+
+        }
+         [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "New Text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFound()
+        {
+            var thing = this._context.Users.Find(-1);
+            if(thing == null)
+            {
+                return NotFound(-1);
+            }
+            return Ok(thing);
+        }
+        [HttpGet("server-error")]
+        public ActionResult<AppUser> GetServerError()
+        {
+            var thing = this._context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+            return Ok(thingToReturn);
+        }
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            return BadRequest("This is a Bad Request");
+        }
+    }
+}
--- DatingAPI/DTO/RegisterDTO.cs
+++ DatingAPI/DTO/RegisterDTO.cs
@@ -12,6 +12,7 @@ namespace DatingAPI.DTO
         public string UserName { get; set; }
 
         [Required]
+        [StringLength(8,MinimumLength =4)]
         public string Password { get; set; }
     }
 }
